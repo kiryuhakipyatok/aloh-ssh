15d4813f3675fbddc8e09fdd0b99590dd0170d1a
kiryuhakipyatok <goodapple5207@gmail.com>
2026-05-23 20:26:04 +0300

    sql fix plz

--- internal/domain/repositories/user-repo.go
+++ internal/domain/repositories/user-repo.go
@@ -177,7 +177,7 @@ func (s *userRepository) NewFriendRequest(ctx context.Context, userId uuid.UUID,
     			INSERT INTO friends (user_id1, user_id2, req_time)
 				SELECT $1, id, $3 FROM found_user
 				ON CONFLICT (LEAST(user_id1, user_id2), GREATEST(user_id1, user_id2)) 
-    			DO UPDATE SET status = 'active' WHERE friends.user_id2 = $1
+    			DO UPDATE SET status = 'active' WHERE friends.user_id2 = $1 AND friends.status = 'pending'
 				RETURNING user_id2
 			)
 			SELECT user_id2 FROM inserted_friend`
