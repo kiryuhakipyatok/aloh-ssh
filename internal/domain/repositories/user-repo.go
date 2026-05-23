ad784873dd3bebb29c7f96506958f3d3c6229657
kiryuhakipyatok <goodapple5207@gmail.com>
2026-05-23 16:21:09 +0300

    sql query fixx

--- internal/domain/repositories/user-repo.go
+++ internal/domain/repositories/user-repo.go
@@ -175,9 +175,9 @@ func (s *userRepository) NewFriendRequest(ctx context.Context, userId uuid.UUID,
 			),
 			inserted_friend AS (
     			INSERT INTO friends (user_id1, user_id2, req_time)
+				SELECT $1, id, $3 FROM found_user
 				ON CONFLICT (LEAST(user_id1, user_id2), GREATEST(user_id1, user_id2)) 
     			DO UPDATE SET status = 'active'
-    			SELECT $1, id, $3 FROM found_user
 			)
 			SELECT id FROM found_user`
 	var id uuid.UUID
