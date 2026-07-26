30dc3a8c5b15ee4a34aaec829619efeb50bda648
kiryuhakipyatok <goodapple5207@gmail.com>
2026-07-26 20:11:10 +0300

    migr

--- internal/migrations/20260720123553_add_users_color.sql
+++ internal/migrations/20260720123553_add_users_color.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 ALTER TABLE IF EXISTS users
-ADD COLUMN color VARCHAR(7) NOT NULL DEFAULT '';
+ADD COLUMN color VARCHAR(7) NOT NULL DEFAULT '#random';
 
 -- +goose Down
 ALTER TABLE IF EXISTS users
